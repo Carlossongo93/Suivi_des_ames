4fb8731dfdda698825774a6b75974f6523eeb4cd
Carlossongo93 <iaeforpeople@gmail.com>
2026-04-21 15:44:55 +0100

    Correction de l'erreur de déployement sping boot sur Railway

---
+++ backend-spring/src/main/java/com/letroupeau/LeTroupeauApplication.java
@@ -0,0 +1,11 @@
+package com.letroupeau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeTroupeauApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeTroupeauApplication.class, args);
+    }
+}
