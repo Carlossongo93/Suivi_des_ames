4ffdabb0044740ed0f29e9a262cd9133204474e0
Carlossongo93 <iaeforpeople@gmail.com>
2026-04-22 14:55:32 +0100

    Mise à jour des clé etrangère au format bigint

--- backend-spring/src/main/resources/db/migration/V2__update_contacts_id_to_bigint.sql
+++ backend-spring/src/main/resources/db/migration/V2__update_contacts_id_to_bigint.sql
@@ -1,1 +1,8 @@
+ALTER TABLE contacts ALTER COLUMN team_id TYPE bigint;
+ALTER TABLE users ALTER COLUMN team_id TYPE bigint;
+ALTER TABLE interactions ALTER COLUMN contact_id TYPE bigint;
+ALTER TABLE interactions ALTER COLUMN user_id TYPE bigint;
+ALTER TABLE teams ALTER COLUMN id TYPE bigint;
+ALTER TABLE users ALTER COLUMN id TYPE bigint;
 ALTER TABLE contacts ALTER COLUMN id TYPE bigint;
+ALTER TABLE interactions ALTER COLUMN id TYPE bigint;
