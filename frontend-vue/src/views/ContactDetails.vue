94cce7164a8f2046994c056e3641e5af28bb0443
Carlossongo93 <iaeforpeople@gmail.com>
2026-05-01 14:31:02 +0100

    Ajout du formulaire interaction 2

--- frontend-vue/src/views/ContactDetails.vue
+++ frontend-vue/src/views/ContactDetails.vue
@@ -66,7 +66,7 @@ import { ref, onMounted } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import api from '../services/api';
 import { ref } from 'vue';
-import InteractionForm from '@/components/InteractionForm.vue'; // Ajustez le chemin
+import InteractionForm from '@/views/InteractionForm.vue'; // Ajustez le chemin
 
 const showForm = ref(false); // Le fameux état qui manquait à l'appel !
 const currentContactId = ref(1); // L'ID du contact actuel
